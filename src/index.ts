349ce3514eb7073c69a06f16ac6be5cbc5c6b566
Anddrrew <59182007+Anddrrew@users.noreply.github.com>
2023-02-08 22:09:35 +0200

    Added cors

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import authMiddleware, { authErrorHandler } from './middlewares/authMiddleware';
 import errorHandler from './middlewares/errorHandler';
 import { port } from './config';
 import RootRouter from './routes';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(authMiddleware);
 
